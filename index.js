784ab997d062bbac542f1ea0d72e23fcd7496579
Mark Protze <46573411+markprotze@users.noreply.github.com>
2020-11-24 15:40:15 -0500

    Remove deprecated and commented out code

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
-
 const app = express();
-
 const cors = require('cors');
-
-
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const expressSession = require('express-session');
-
-
-
 const cors_options = {
     origin: 'https://gdasd.github.io',
     credentials: true
@@ -51,15 +43,12 @@ app.post('/user', (req, res) => {
 app.post('/login', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
-    // console.log(req.body);
     let user_data = login_data.get(username);
-    // console.log(user_data)
     if (user_data.username == null) {
         res.status(404).send("User not found")
         return;
     }
     if (user_data.password == password) {
-        // console.log("User " + username + " credentials valid")
         req.session.user = username;
         res.json(username)
         return;
@@ -74,49 +63,23 @@ app.get('/logout', (req, res) => {
 })
 
 app.put('/userscore/:username', (req, res) => {
-    //console.log("we in");
-  /*  if (req.session.user == undefined) {
-        // console.log("here")
-        res.status(403).send("Unauthorized no one logged in")
-        return;
-    }*/
     let upUser = User.findByName(req.params.username);
-    // console.log(upUser);
     if (upUser == null) {
         res.status(404).send("User not found");
         return;
     }
-
-  /*  if(upUser.username != req.session.user) {
-        res.status(403).send("Unauthorized user not logged in")
-        return;
-    }*/
-    
     upUser.updateScore(req.body.score);
     res.json(upUser);
 })
 
 app.post('/userpass/:username', (req, res) => {
-    //console.log("we in");
-  /*  if (req.session.user == undefined) {
-        // console.log("here")
-        res.status(403).send("Unauthorized no one logged in")
-        return;
-    }*/
     let user_data = login_data.get(req.params.username);
     let upUser = User.findByName(req.params.username);
-    // console.log(upUser);
     if (upUser == null) {
         res.status(404).send("User not found");
         return;
     }
-
-  /*  if(upUser.username != req.session.user) {
-        res.status(403).send("Unauthorized user not logged in")
-        return;
-    }*/
     if (user_data.password != req.body.currentPassword) {
-        // console.log("User " + username + " credentials valid")
         res.status(403).send("Wrong Password");
         return;
     }
@@ -149,16 +112,8 @@ app.delete('/user/:username', (req, res) => {
 
 
 app.get('/users', (req, res) => {
-    
-   /* if (req.session.user == undefined) {
-        res.status(403).send("Unauthorized")
-        return;
-    }*/
-
     // to give you users for the leaderboard, I'll sort the users by score (descending) and send it
-    // console.log("we here")
     let sortedUsers = User.getAllUsers()
-    // console.log(sortedUsers)
     sortedUsers = sortedUsers.sort((a, b) => parseInt(b.score) - parseInt(a.score));
     res.json(sortedUsers);
     return;
@@ -169,33 +124,8 @@ app.get('/username', (req, res) => {
         res.status(403).send("unauthorized")
         return;
     }
-    
     res.json(req.session.user);
 })
-
-
-/*app.get('/user/:username', (req, res) => {
-    if (req.session.user == undefined) {
-        res.status(403).send("Unauthorized no none logged in")
-        return;
-    }
-
-    let user = User.findByName(req.params.username);
-
-    if (user == null) {
-        res.status(404).send("User not found");
-        return;
-    }
-
-    if(user.username != req.session.user) {
-        res.status(403).send("Unauthorized user not logged in")
-        return;
-    }
-
-    res.json(user);
-})*/
-
-
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
